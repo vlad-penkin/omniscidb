heavyai/heavydb

Adding conda environment for towards reproducible build Derived from conda-forge/omniscidb-cpu-feedstock but moving  all the workarounds into CMakeList.txt. Added target that help preventing races before running main build.

SampleData/db-query-list-flights.sql
@@ -1,0 +1,38 @@
+USER admin omnisci {
+
+DROP TABLE IF EXISTS flights;
+
+CREATE DATAFRAME flights(
+    Year smallint,
+    Month smallint,
+    DayofMonth smallint,
+    DayOfWeek smallint,
+    DepTime smallint,
+    CRSDepTime smallint,
+    ArrTime smallint,
+    CRSArrTime smallint,
+    UniqueCarrier text encoding dict,
+    FlightNum smallint,
+    TailNum text encoding dict,
+    ActualElapsedTime smallint,
+    CRSElapsedTime smallint,
+    AirTime smallint,
+    ArrDelay smallint,
+    DepDelay smallint,
+    Origin text encoding dict,
+    Dest text encoding dict,
+    Distance smallint,
+    TaxiIn smallint,
+    TaxiOut smallint,
+    Cancelled smallint,
+    CancellationCode text encoding dict,
+    Diverted smallint,
+    CarrierDelay smallint,
+    WeatherDelay smallint,
+    NASDelay smallint,
+    SecurityDelay smallint,
+    LateAircraftDelay smallint)
+    FROM 'CSV:SampleData/100_flights.csv' WITH(header='true');
+
+    select count(*) from flights where LateAircraftDelay >= 10;
+}

vlad-penkin/omniscidb

SampleData/db-query-list-flights.sql
@@ -1,0 +1,38 @@
+USER admin omnisci {
+
+DROP TABLE IF EXISTS flights;
+
+CREATE TEMPORARY TABLE flights(
+    Year_flight smallint,
+    Month_flight smallint,
+    DayofMonth smallint,
+    DayOfWeek smallint,
+    DepTime smallint,
+    CRSDepTime smallint,
+    ArrTime smallint,
+    CRSArrTime smallint,
+    UniqueCarrier text encoding dict,
+    FlightNum smallint,
+    TailNum text encoding dict,
+    ActualElapsedTime smallint,
+    CRSElapsedTime smallint,
+    AirTime smallint,
+    ArrDelay smallint,
+    DepDelay smallint,
+    Origin text encoding dict,
+    Dest text encoding dict,
+    Distance smallint,
+    TaxiIn smallint,
+    TaxiOut smallint,
+    Cancelled smallint,
+    CancellationCode text encoding dict,
+    Diverted smallint,
+    CarrierDelay smallint,
+    WeatherDelay smallint,
+    NASDelay smallint,
+    SecurityDelay smallint,
+    LateAircraftDelay smallint)
+    FROM "CSV:SampleData/100_flights.csv";
+
+    select count(*) from flights where LateAircraftDelay >= 10;
+}