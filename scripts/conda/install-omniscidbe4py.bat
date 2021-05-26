bfca3187222fef5fc8c918da14ccec631ff2fbfe
izamyati <igor.zamyatin@intel.com>
2021-05-26 16:34:09 -0500

    windows script for package installing

---
+++ scripts/conda/install-omniscidbe4py.bat
@@ -0,0 +1,10 @@
+cmake --install . --component "include" --prefix %CONDA_PREFIX%/include/omnisci
+cmake --install . --component "doc" --prefix %CONDA_PREFIX%/share/doc/omnisci
+cmake --install . --component "data" --prefix %CONDA_PREFIX%/opt/omnisci
+cmake --install . --component "thrift" --prefix %CONDA_PREFIX%/opt/omnisci
+cmake --install . --component "QE" --prefix %CONDA_PREFIX%
+cmake --install . --component "jar" --prefix %CONDA_PREFIX%
+cmake --install . --component "Unspecified" --prefix %CONDA_PREFIX%/opt/omnisci
+cmake --install . --component "exe" --prefix %CONDA_PREFIX%
+cmake --install . --component "DBE" --prefix %CONDA_PREFIX%
+python Embedded\setup.py build_ext -f install
