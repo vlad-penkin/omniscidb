c7c82bd0c5b90f58740636daabe8160539f6e10f
Gleb Novichkov <gleb.novichkov@intel.com>
2021-12-24 01:53:54 +0300

    AVX512 bitmap creation implemented (with some limitations)

--- bitmap.cpp
+++ bitmap.cpp
@@ -1,6 +1,7 @@
 #include<atomic>
 #include<bitset>
 #include<cassert>
+#include<cfloat>
 #include<cstdint>
 #include<cstring>
 #include<chrono>
@@ -8,9 +9,23 @@
 #include<iostream>
 #include<vector>
 #include<x86intrin.h>
+#include<iomanip>
 
-//  TBB
+
+#include <immintrin.h>
+#include <nmmintrin.h>
+#include <xmmintrin.h>
+
+//  tbb
 #include<tbb/parallel_for.h>
+
+//  boost
+#include<boost/align/aligned_allocator.hpp>
+
+//  External dependences
+extern "C" size_t gen_bitmap_8(uint8_t *bitmap, size_t *null_count, uint8_t *data, size_t size, uint64_t null_val);
+extern "C" size_t gen_bitmap_32(uint8_t *bitmap, size_t *null_count, uint32_t *data, size_t size, uint64_t null_val);
+extern "C" size_t gen_bitmap_64(uint8_t *bitmap, size_t *null_count, uint64_t *data, size_t size, uint64_t null_val);
 
 // CC: 
 //  g++ -O3 -std=c++17 bitmap.cpp -I /localdisk2/gnovichk/miniconda3/envs/omnisci-dev/include -L /localdisk2/gnovichk/miniconda3/envs/omnisci-dev/lib -ltbb -o bitmap 
@@ -18,23 +33,65 @@ size_t computeBitmapSize(size_t data_size) {
     return (data_size + 7) / 8;    
 }
 
+template <class TYPE> constexpr inline TYPE inline_int_null_value() {
+  return std::is_signed<TYPE>::value 
+            ? std::numeric_limits<TYPE>::min()
+            : std::numeric_limits<TYPE>::max();
+}
+
+template <class TYPE> constexpr inline TYPE inline_fp_null_value();
+
+template <> constexpr inline float inline_fp_null_value<float>() {
+  return FLT_MIN;
+}
+
+template <> constexpr inline double inline_fp_null_value<double>() {
+  return DBL_MIN;
+}
+
+template <typename TYPE> constexpr TYPE null_builder() {
+  static_assert(std::is_floating_point_v<TYPE> || std::is_integral_v<TYPE>,
+                "Unsupported type");
+
+  if constexpr (std::is_floating_point_v<TYPE>) {
+    return inline_fp_null_value<TYPE>();
+  } else if constexpr (std::is_integral_v<TYPE>) {
+    return inline_int_null_value<TYPE>();
+  }
+}
+
+template<typename TYPE>
+std::string get_type_name() {
+    if constexpr (std::is_same<TYPE,  char>::value)  return "char";
+    else if constexpr (std::is_same<TYPE,  int8_t>::value)  return "int8_t";
+    else if constexpr (std::is_same<TYPE,  uint8_t>::value) return "uint8_t";
+    else if constexpr (std::is_same<TYPE,  int32_t>::value) return "int32_t";
+    else if constexpr (std::is_same<TYPE, uint32_t>::value) return "uint32_t";
+    else if constexpr (std::is_same<TYPE,  int64_t>::value) return "int64_t";
+    else if constexpr (std::is_same<TYPE, uint64_t>::value) return "uint64_t";
+    else if constexpr (std::is_same<TYPE,  double>::value) return "double";
+    else if constexpr (std::is_same<TYPE,  float>::value)  return "float";
+    else {
+        throw std::runtime_error ("get_type_name() -- unsupported type");
+    }
+}
 
 void printBitmap(const std::vector<uint8_t>& bitmap_data, bool reverse = true)
 {
     for (auto & c: bitmap_data) {
         std::string s = std::bitset<8>(c).to_string();
-        if (reverse) {
-            std::reverse (std::begin(s), std::end(s));
-        }
+        if (reverse) { std::reverse (std::begin(s), std::end(s)); }
         std::cout << s << " ";
     }
     std::cout << std::endl;
 }
 
 template <typename TYPE = int32_t>
-void createBitmapParallelFor(std::vector<uint8_t>& bitmap_data, int64_t& null_count_out, const std::vector<TYPE>& vals) 
+void createBitmapParallelFor(std::vector<uint8_t>& bitmap_data, 
+                             int64_t& null_count_out, 
+                             const std::vector<TYPE>& vals,
+                             TYPE null_val = null_builder<TYPE>() ) 
 {
-    auto null_val = std::numeric_limits<TYPE>::max();
     size_t chunk_rows_count = vals.size();
     size_t unroll_count = chunk_rows_count & 0xFFFFFFFFFFFFFFF8ULL;
 
@@ -99,9 +156,11 @@ void createBitmapParallelFor(std::vector<uint8_t>& bitmap_data, int64_t& null_co
 }
 
 template <typename TYPE = int32_t>
-void createBitmap(std::vector<uint8_t>& bitmap_data, int64_t& null_count_out, const std::vector<TYPE>& vals) 
+void createBitmap(std::vector<uint8_t>& bitmap_data, 
+                  int64_t& null_count_out, 
+                  const std::vector<TYPE>& vals,
+                  TYPE null_val = null_builder<TYPE>()) 
 {
-    auto null_val = std::numeric_limits<TYPE>::max();
     size_t chunk_rows_count = vals.size();
     size_t unroll_count = chunk_rows_count & 0xFFFFFFFFFFFFFFF8ULL;
 
@@ -163,19 +222,19 @@ void test1() {
     printBitmap(data, false);
 }
 
-void profile(size_t size) {
-    std::vector<int32_t> values(size, 0);
-    values[0] = std::numeric_limits<int32_t>::max();
-    size_t bitmap_size = computeBitmapSize(values.size());
+// void profile(size_t size) {
+//     std::vector<int32_t> values(size, 0);
+//     values[0] = std::numeric_limits<int32_t>::max();
+//     size_t bitmap_size = computeBitmapSize(values.size());
 
-    int64_t null_count = 0;
-    std::vector<uint8_t> bitmap_data(bitmap_size, 0);
+//     int64_t null_count = 0;
+//     std::vector<uint8_t> bitmap_data(bitmap_size, 0);
 
-    auto start = std::chrono::high_resolution_clock::now();
-    createBitmap(bitmap_data, null_count, values);
-    size_t dur = std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::high_resolution_clock::now()-start).count(); 
-    std::cout << "[DEFAULT] Elapsed, usec: " << dur << std::endl;
-}
+//     auto start = std::chrono::high_resolution_clock::now();
+//     createBitmap(bitmap_data, null_count, values);
+//     size_t dur = std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::high_resolution_clock::now()-start).count(); 
+//     std::cout << "[DEFAULT] Elapsed, usec: " << dur << std::endl;
+// }
 
 void profile_tbb(size_t size) {
     std::vector<int32_t> values(size, 0);
@@ -213,6 +272,271 @@ size_t diffBitmap(std::vector<uint8_t>& bm1, std::vector<uint8_t>& bm2)
     return diff_count;
 }
 
+
+template<typename TYPE>
+size_t gen_bitmap(uint8_t *bitmap, size_t *null_count, TYPE *data, size_t size)
+{
+    if constexpr (std::is_same<TYPE,  int8_t>::value) {
+        return gen_bitmap_8(bitmap, null_count, reinterpret_cast<uint8_t*>(data), size, 0x8080808080808080);
+    }
+    else if constexpr (std::is_same<TYPE,  uint8_t>::value) {
+        return gen_bitmap_8(bitmap, null_count, reinterpret_cast<uint8_t*>(data), size, 0xFFFFFFFFFFFFFFFF);
+    }
+    else if constexpr (std::is_same<TYPE,  int32_t>::value) {
+        return gen_bitmap_32(bitmap, null_count, reinterpret_cast<uint32_t*>(data), size, 0x8000000080000000);
+    }
+    else if constexpr (std::is_same<TYPE,  uint32_t>::value) {
+        return gen_bitmap_32(bitmap, null_count, reinterpret_cast<uint32_t*>(data), size, 0xFFFFFFFFFFFFFFFF);
+    }
+    else if constexpr (std::is_same<TYPE,  int64_t>::value) {
+            return gen_bitmap_64(bitmap, null_count, reinterpret_cast<uint64_t*>(data), size, null_builder<int64_t>());
+    }
+    else if constexpr (std::is_same<TYPE,  uint64_t>::value) {
+            return gen_bitmap_64(bitmap, null_count, reinterpret_cast<uint64_t*>(data), size, null_builder<uint64_t>());
+    }
+    if constexpr (std::is_same<TYPE,float>::value) {
+            return gen_bitmap_32(bitmap, null_count, reinterpret_cast<uint32_t*>(data), size, 0x0080000000800000);
+    }
+    if constexpr (std::is_same<TYPE,double>::value) {
+            return gen_bitmap_64(bitmap, null_count, reinterpret_cast<uint64_t*>(data), size, 0x0010000000000000);
+    }
+    else {
+        throw std::runtime_error ("gen_bitmap() -- Unsupported type: " + get_type_name<TYPE>() + ". Aborting.");
+    }
+}
+
+
+
+template<typename TYPE>
+void test(size_t size) {
+
+    size_t items_per_iteration = 64/sizeof(TYPE);
+
+    if (size % items_per_iteration != 0) {
+        throw std::runtime_error ("Provided size ("+std::to_string(size)
+                    +") is not a multiple of "+std::to_string(items_per_iteration)+".  Aborting.");
+    }
+
+    size_t actual_null_count;
+    int64_t test_null_count;
+    TYPE null_value = null_builder<TYPE>();
+
+    std::vector<TYPE, boost::alignment::aligned_allocator<TYPE, 64>> 
+        nulldata (size, 0);
+
+    size_t bitmap_size = computeBitmapSize(nulldata.size());
+    std::vector<uint8_t> actual_bitmap_data(bitmap_size, 0);
+    std::vector<uint8_t> test_bitmap_data(bitmap_size, 0);
+
+    //  populating nulldata
+    for (size_t i=0; i<nulldata.size(); i++) {
+        nulldata[i] = (i%2 == 0) ? 0 : null_value;
+    }
+
+    // std::cout << get_type_name<TYPE>() <<"; " << std::bitset<sizeof(TYPE)*8> (nulldata[0]) << std::dec << std::endl;
+    // std::cout << get_type_name<TYPE>() <<"; " << std::bitset<sizeof(TYPE)*8> (reinterpret_cast<uint64_t&>(nulldata[1])) << std::dec << std::endl;
+    // std::cout << get_type_name<TYPE>() <<"; " << std::hex << reinterpret_cast <uint64_t&>(nulldata[1]) << std::dec << std::endl;
+
+    std::vector<TYPE> test_nulldata(nulldata.begin(), nulldata.end());
+
+    createBitmap(test_bitmap_data, test_null_count, test_nulldata, null_value);
+    gen_bitmap(actual_bitmap_data.data(), &actual_null_count, nulldata.data(), nulldata.size());
+
+    std::cout <<get_type_name<TYPE>() <<"; Expected bitmap: "; printBitmap(test_bitmap_data, true);
+    std::cout <<get_type_name<TYPE>() <<"; Actual bitmap:   "; printBitmap(actual_bitmap_data, true);
+
+    std::cout <<get_type_name<TYPE>() <<"; NULL COUNT: expected: " << test_null_count << ", actual: " << actual_null_count << std::endl;
+}
+
+
+
+
+template<typename TYPE>
+void profile_avx512(size_t size, size_t num_iter = 200) {
+    size_t items_per_iteration = 64/sizeof(TYPE);
+
+    if (size % items_per_iteration != 0) {
+        throw std::runtime_error ("Provided size ("+std::to_string(size)
+                    +") is not a multiple of "+std::to_string(items_per_iteration)+".  Aborting.");
+    }
+
+    std::vector<TYPE, boost::alignment::aligned_allocator<TYPE, 64>> 
+        nulldata(size,0);
+
+    //  populating nulldata
+    for (size_t i=0; i<nulldata.size(); i++) {
+        nulldata[i] = (i%2 == 0) ? 0 : null_builder<TYPE>();
+    }
+
+
+    size_t bitmap_size = computeBitmapSize(nulldata.size());
+    std::vector<uint8_t> bitmap_data(bitmap_size, 0);
+    size_t null_count = 0;
+
+
+    auto start = std::chrono::high_resolution_clock::now();
+    for (int i = 0; i<num_iter/sizeof(TYPE); i++) {
+        gen_bitmap(bitmap_data.data(), &null_count, nulldata.data(), nulldata.size());
+    }
+    size_t dur = std::chrono::duration_cast<std::chrono::nanoseconds>(std::chrono::high_resolution_clock::now()-start).count(); 
+
+    std::cout 
+        << "[AVX512];\tSource size: " << std::setw(12) <<std::right << size 
+        << ", type: " << std::setw(8) << get_type_name<TYPE>()
+        << " (" << std::setw(0) << sizeof(TYPE) <<" byte)"
+        << ", elapsed time, usec: " << std::setw(7) << std::setprecision(6) << std::left << dur/1000.0/num_iter << std::endl;
+}
+
+template<typename TYPE>
+void profile_default(size_t size, size_t num_iter = 200) {
+    size_t items_per_iteration = 64/sizeof(TYPE);
+
+    if (size % items_per_iteration != 0) {
+        throw std::runtime_error ("Provided size ("+std::to_string(size)
+                    +") is not a multiple of "+std::to_string(items_per_iteration)+".  Aborting.");
+    }
+
+    std::vector<TYPE/*, boost::alignment::aligned_allocator<TYPE, 64>*/>
+        nulldata(size,0);
+
+    for (size_t i=0; i<nulldata.size(); i++) {
+        nulldata[i] = (i%2 == 0) ? 0 : null_builder<TYPE>();
+    }
+
+    size_t bitmap_size = computeBitmapSize(nulldata.size());
+    std::vector<uint8_t> bitmap_data(bitmap_size, 0);
+    int64_t null_count = 0;
+
+
+    auto start = std::chrono::high_resolution_clock::now();
+    for (int i = 0; i<num_iter/sizeof(TYPE); i++) {
+        createBitmap(bitmap_data, null_count, nulldata);
+    }
+    size_t dur = std::chrono::duration_cast<std::chrono::nanoseconds>(std::chrono::high_resolution_clock::now()-start).count(); 
+
+    std::cout 
+        << "[DEFAULT];\tSource size: " << std::setw(12) <<std::right << size 
+        << ", type: " << std::setw(8) << get_type_name<TYPE>()
+        << " (" << std::setw(0) << sizeof(TYPE) <<" byte)"
+        << ", elapsed time, usec: " << std::setw(7) << std::setprecision(6) << std::left << dur/1000.0/num_iter << std::endl;
+}
+
+// void profile(size_t size) {
+//     std::vector<int32_t> values(size, 0);
+//     values[0] = std::numeric_limits<int32_t>::max();
+//     size_t bitmap_size = computeBitmapSize(values.size());
+
+//     int64_t null_count = 0;
+//     std::vector<uint8_t> bitmap_data(bitmap_size, 0);
+
+//     auto start = std::chrono::high_resolution_clock::now();
+//     createBitmap(bitmap_data, null_count, values);
+//     size_t dur = std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::high_resolution_clock::now()-start).count(); 
+//     std::cout << "[DEFAULT] Elapsed, usec: " << dur << std::endl;
+// }
+
+
+
+void major_test()
+{
+    test<int8_t>(64);
+    test<uint8_t>(64);
+    test<int32_t>(64);
+    test<uint32_t>(64);
+    test<int64_t>(64);
+    test<uint64_t>(64);
+    test<float>(64);
+    test<double>(64);
+}
+
+void major_profile()
+{
+    std::cout << "\nAVX512 implementation profiling (single thread)\n";
+
+    profile_avx512<int8_t>(30'000'000);
+    profile_avx512<uint8_t>(30'000'000);
+
+    profile_avx512<int32_t>(30'000'000);
+    profile_avx512<uint32_t>(30'000'000);
+
+    profile_avx512<int64_t>(30'000'000);
+    profile_avx512<uint64_t>(30'000'000);
+
+    profile_avx512<float>(30'000'000);
+    profile_avx512<double>(30'000'000);
+
+    std::cout << "\nThe usual C++ implementation profiling (single thread)\n";
+    //  default (cpu) implementation profiling
+    profile_default<int8_t>(30'000'000);
+    profile_default<uint8_t>(30'000'000);
+
+    profile_default<int32_t>(30'000'000);
+    profile_default<uint32_t>(30'000'000);
+
+    profile_default<int64_t>(30'000'000);
+    profile_default<uint64_t>(30'000'000);
+
+    profile_default<float>(30'000'000);
+    profile_default<double>(30'000'000);
+}
+
+int main() try {
+    major_test();
+    major_profile();
+    return 0;
+}
+catch (std::runtime_error & e) {
+    std::cout << e.what() << std::endl;
+    return 255;
+}
+
+
+
+int test2()
+{
+    size_t size = 16;
+    std::vector<int32_t> values(size, 0);
+    values[0] = std::numeric_limits<int32_t>::max();
+    size_t bitmap_size = computeBitmapSize(values.size());
+    std::vector<uint8_t> bitmap_data(bitmap_size, 0);
+    std::cout << "Bitmap size: " << bitmap_data.size() << std::endl;
+    int64_t null_count = 0;
+    createBitmap(bitmap_data, null_count, values);
+    std::cout << "Nulls count: " << null_count << std::endl;
+    printBitmap(bitmap_data, true);
+
+    std::cout << "Running diffBitmap on the same data\n";
+    diffBitmap(bitmap_data, bitmap_data);
+
+    std::cout << "Running diffBitmap on the different bitmap data\n";
+    std::vector<uint8_t> test_bitmap_data (bitmap_data);
+    test_bitmap_data [0] = 0;
+    diffBitmap(bitmap_data, test_bitmap_data);
+
+
+    // for (int i = 0; i<10; i++) {
+    //     profile(30'000'000);
+    // }
+
+    // for (int i = 0; i<10; i++) {
+    //     profile_tbb(30'000'000);
+    // }    
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // It's assumed sample has 64 extra bytes to handle alignment.
 __attribute__((target("avx512f"))) void spread_vec_sample(uint8_t* dst,
                                                           const size_t dst_size,
@@ -244,94 +568,4 @@ __attribute__((target("avx512f"))) void spread_vec_sample(uint8_t* dst,
 
   // Scalar tail.
   memcpy(align_dst, align_sample, rem_scalar);
-}
-
-#include <immintrin.h>
-#include <nmmintrin.h>
-#include <xmmintrin.h>
-
-
-
-extern "C" size_t gen_bitmap_8(uint8_t *bitmap, size_t *null_count, uint8_t *data, size_t size);
-//__v64qi 
-
-int8_t  v0_8[64] =  {-1, 0, 0, -1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-                      0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-                      0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-                      0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, -1, 0, -1};
-
-int8_t v1_8[128] = {-1, 0, 0, -1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-                     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-                     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-                     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, -1, 0, -1,
-                    -1, 0, 0, -1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-                     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-                     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-                     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, -1, 0, -1
-                   };
-
-
-void test_byte_64() {
-    size_t null_count;
-    std::vector<uint8_t> bm_data(64/8);
-    gen_bitmap_8(bm_data.data(), &null_count, reinterpret_cast<uint8_t*>(&v0_8), 64);
-    std::cout << "Nulls count: " << null_count << std::endl;
-    printBitmap(bm_data, true);
-}
-void test_byte_128() {
-    size_t null_count;
-    std::vector<uint8_t> bm_data(2*64/8);
-    gen_bitmap_8(bm_data.data(), &null_count, reinterpret_cast<uint8_t*>(&v1_8), 2*64);
-    std::cout << "Nulls count: " << null_count << std::endl;
-    printBitmap(bm_data, true);
-}
-
-int main()
-{
-    test_byte_64();
-    test_byte_128();
-
-    size_t null_count;
-    std::vector<uint8_t> bm_data(128/8);
-
-
-    auto start = std::chrono::high_resolution_clock::now();
-    for (int i = 0; i<100000; i++) {
-        gen_bitmap_8(bm_data.data(), &null_count, reinterpret_cast<uint8_t*>(&v1_8), 128);
-    }
-    size_t dur = std::chrono::duration_cast<std::chrono::nanoseconds>(std::chrono::high_resolution_clock::now()-start).count(); 
-    std::cout << "[AVX512] Elapsed, ns: " << dur/100000.0 << std::endl;
-}
-
-
-int test2()
-{
-    size_t size = 16;
-    std::vector<int32_t> values(size, 0);
-    values[0] = std::numeric_limits<int32_t>::max();
-    size_t bitmap_size = computeBitmapSize(values.size());
-    std::vector<uint8_t> bitmap_data(bitmap_size, 0);
-    std::cout << "Bitmap size: " << bitmap_data.size() << std::endl;
-    int64_t null_count = 0;
-    createBitmap(bitmap_data, null_count, values);
-    std::cout << "Nulls count: " << null_count << std::endl;
-    printBitmap(bitmap_data, true);
-
-    std::cout << "Running diffBitmap on the same data\n";
-    diffBitmap(bitmap_data, bitmap_data);
-
-    std::cout << "Running diffBitmap on the different bitmap data\n";
-    std::vector<uint8_t> test_bitmap_data (bitmap_data);
-    test_bitmap_data [0] = 0;
-    diffBitmap(bitmap_data, test_bitmap_data);
-
-
-    for (int i = 0; i<10; i++) {
-        profile(30'000'000);
-    }
-
-    for (int i = 0; i<10; i++) {
-        profile_tbb(30'000'000);
-    }    
-    return 0;
 }
